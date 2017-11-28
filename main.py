4f62c843fe8ac97a63af049c5274850e5e83431b
Nozban <etienne.brangbour@gmail.com>
2017-11-28 17:56:29 +0100

    loul
    louloulou

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from detector import detect
 from teamBuilder import teambuilder
 import keyboard
 
+k = keyboard.read_key()
+print(k.char)
+if k=='1 down':
+	print(k)
+
 oppteam = detect()
 if oppteam == 'detection error':
 	print(oppteam)
